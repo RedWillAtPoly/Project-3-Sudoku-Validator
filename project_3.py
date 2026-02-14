cdb7e2a424146ba5227d2da80359a37cb699cb32
Will Redekopp <wredekop@calpoly.edu>
2026-02-14 13:38:53 -0800

    square checker function and complete checker complete, but initial test shows failure

--- project_3.py
+++ project_3.py
@@ -39,3 +39,47 @@ def all_columns_checker(grid):
         column_list = []
     return True
 
+#checks a certain 3x3 given grid
+def check_3_square(grid ,min_row_index, min_col_index):
+    max_row_index = min_row_index + 2
+    max_col_index = min_col_index + 2
+    vals_in_square = []
+    #gets the row
+    for i in range(min_row_index, min_row_index + 3):
+        for j in range(min_col_index, min_col_index + 3):
+            vals_in_square.append(grid[i][j])
+    vals_in_square.sort()
+    if vals_in_square != [1,2,3,4,5,6,7,8,9]:
+        return False
+    return True
+
+#Combined solution validator
+def checker(puzzle):
+    if not check_dimensions(puzzle):
+        return False
+    if not validate_data(puzzle):
+        return False
+    if not all_rows_checker(puzzle):
+        return False
+    if not all_columns_checker(puzzle):
+        return False
+    corner_index_list = [0,3,6]
+    for row_corner in corner_index_list:
+        for col_corner in corner_index_list:
+            if not check_3_square(puzzle, row_corner, col_corner):
+                return False
+    return True
+
+valid_board = [
+  [5,3,4,6,7,8,9,1,2],
+  [6,7,2,1,9,5,3,4,8],
+  [1,9,8,3,4,2,5,6,7],
+  [8,5,9,7,6,1,4,2,3],
+  [4,2,6,8,5,3,7,9,1],
+  [7,1,3,9,2,4,8,5,6],
+  [9,6,1,5,3,7,2,8,4],
+  [2,8,7,4,1,9,6,3,5],
+  [3,4,5,2,8,6,1,7,9]
+]
+if not checker(valid_board):
+    print('all good')
